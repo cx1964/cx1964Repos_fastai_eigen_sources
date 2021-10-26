2ced766759df30e2907cba27b54c9aea9ddc5beb
cx1964 <cx1964@mail.com>
2021-10-26 14:07:50 +0200

    script om identity in te stellen

---
+++ 01_set_git_identity.sh
@@ -0,0 +1,6 @@
+# Filenaam: 01_set_git_identity.sh
+# Functie: Leg in git config file je identiteit vast
+
+git config --global user.email "cx1964@mail.com"
+git config --global user.name "cx1964"
+git config --global core.editor "code --wait"
