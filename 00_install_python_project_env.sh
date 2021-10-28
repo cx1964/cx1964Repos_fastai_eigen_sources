9a70d40cc4b7839cef67310f4e6c9a404a16d254
cx1964 <cx1964@mail.com>
2021-10-28 20:06:24 +0200

    werkende installatie script

--- 00_install_python_project_env.sh
+++ 00_install_python_project_env.sh
@@ -14,7 +14,7 @@
 # kies een python versie mbv
 # pyenv install --list
 # Nog onbekend hoe een versie te installeren hoger dan de system python versie op het OS
-export PYTHON_VERSION="3.8.10" 
+export PYTHON_VERSION="3.8.10" # "3.6-7.3.3"
 ### Einde aanpassen ###
 
 
@@ -47,22 +47,26 @@ else
   pipenv --python $PYTHON_VERSION
 
   ### Begin aanpassen ###
-    
-  pipenv lock --clear
-  pipenv install jupyter notebook
-  # Een van onderstaande regels mbt jupyter_contrib_nbextensions krijg ik nog niet geinstalleerd
-  # install jupyter_contrib_nbextensions https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/install.html
-  # pipenv install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
-  pipenv install jupyter_contrib_nbextensions
+  # zie https://docs.fast.ai/#Installing
+  git clone https://github.com/fastai/fastai1
+  pipenv run pip install --no-deps fastai
+  cd fastai1
+  pipenv run python tools/run-after-git-clone
+  pipenv run python setup.py -q deps
+  # men kan ook minder fastai dependencies installeren bv --dep-groups=core,vision
+  pipenv install $(python setup.py -q deps --dep-groups=core,text,vision)
   
-   # alle benodigde python packages die geinstalleerd moet worden in de env 
-  pipenv install fastbook
+  pipenv install jupyter notebook jupyter_contrib_nbextensions
+  # info mbt jupyter_contrib_nbextensions
+  # https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/install.html
+   
   # Op Ubuntu 20.04 nodig ivm warning mbt missing graphviz
   pipenv install graphviz
   ## Einde aanpassen ###
 
   # Toon geinstalleerde modules 
   ###pipenv run pip list
+
   return 0
 fi
 
